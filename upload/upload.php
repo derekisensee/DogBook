ad93b614bf24e398159900816f4689bb9d62c255
Derek Isensee <derek.isensee@gmail.com>
2016-08-25 11:55:25 -0500

    Create upload.php

---
+++ upload/upload.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1>DogBook Upload</h1>
+    </head>
+    
+    <body>
+        <button class="btn-primary">Choose image to upload.</button>
+    </body>
+</html>
