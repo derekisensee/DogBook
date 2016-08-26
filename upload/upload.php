26301540e3e5969ad528e0eda8dab8eaaa2b9622
falldj <derek.isensee@gmail.com>
2016-08-25 21:30:18 -0500

    started work on uploading process
    after this is done: figure out how to display multiple images from
    folder. decide how often to wipe images.

--- upload/upload.php
+++ upload/upload.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <h1>DogBook Upload</h1>
+        <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+        <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css'>
+        <link rel="stylesheet" href="css/upload.css">
+        <div class="container">
+            <h1>DogBook Upload</h1>
+        </div>
+        <hr>
     </head>
     
     <body>
-        <button class="btn-primary">Choose image to upload.</button>
+        <div class="row">
+            <div class="col-sm-5">
+                <div class="input-group">
+                    <form enctype="multipart/form-data" action="*.php" method="post">
+                        <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+                        <input name="userfile" type="file" />
+                        <input type="submit" value="Upload Image" />
+                    </form>
+                </div>
+            </div>
+        </div>
+        <button class="btn-primary"></button>
     </body>
 </html>
